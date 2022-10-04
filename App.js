4a21b0b299e29fb420d3fa84994322433bad7d80
Yakub Shakirudeen olaide <yakubshakirudeenolaide2018@gmail.com>
2022-10-04 13:14:23 +0100

    Create App.js

---
+++ App.js
@@ -0,0 +1,18 @@
+<script>
+var o=document.getElementById("others");
+var y=document.getElementById("me");
+   function myFunction(x){
+   if(x.classList.toggle("change")){
+     y.style.display="block";
+   }else{
+    y.style.display="none";
+
+      }
+    }
+var q= document.getElementById("btny");
+     function me (){
+     if(q.click=true){
+    var m= confirm("You are being redirect to our whatshapp store");
+ }    
+}
+</script
