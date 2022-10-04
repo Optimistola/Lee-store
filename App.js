d2ec6226897493f9681c35ea81c57c2bea8d77bb
Yakub Shakirudeen olaide <yakubshakirudeenolaide2018@gmail.com>
2022-10-04 15:05:11 +0100

    Update App.js

--- App.js
+++ App.js
@@ -1,20 +1,20 @@
 
 
-   function myFunction(x){
- Let o=document.getElementById("others");
-  Let y=document.getElementById("me");
-   if(x.classList.toggle("change")){
-     y.style.display="block";
-   }else{
-    y.style.display="none";
+   
+var o=document.getElementById("others");
+var y=document.getElementById("me");
+function myFunction(x){
+if(x.classList.toggle("change")){
+y.style.display="block";
+}
+else{
+y.style.display="none";
 
-      }
-    }
-
-     function me (){
-  Let q= document.getElementById("btny");
-     if(q.click=true){
-    var m= confirm("You are being redirect to our whatshapp store");
- }    
-  }
-
+}
+}
+var q= document.getElementById("btny");
+function me (){
+if(q.click=true){
+var m= confirm("You are being redirect to our whatshapp store");
+}
+}
